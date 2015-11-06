c0beb86b449fdbef02430ab386e07e4118c46068
Thiago do Prado Silva <thiagosemjoelho@gmail.com>
2015-11-06 14:05:30 -0200

    Testando o Corredor
    -Criação de uma área de trabalho pra testar o player;

--- A Vassoura Dourada/runner.js
+++ A Vassoura Dourada/runner.js
@@ -4,29 +4,29 @@ var Runner = function() {
 	this.w = 126;
 	this.h = 126;
 	this.weight = 12;
-	this.speed_x = 8;
-	this.speed_y = 12;
-	this.espadada_w = 114;
-	this.espadada_h = 132;
-	this.colisao_x = 133;
-	this.colisao_y = 385;
-	this.colisao_w = 84;
-	this.colisao_h = 116;
+	this.speedX = 8;
+	this.speedY = 12;
+	this.swordW = 114;
+	this.swordH = 132;
+	this.colliderX = 133;
+	this.colliderY = 385;
+	this.colliderW = 84;
+	this.colliderH = 116;
 	
-	this.moveup = false;
-	this.moveup1 = false;
-	this.pulo_case = true;
-	this.pulo_case2 = true;
-	this.espadada = false;
+	this.moveUp = false;
+	this.moveUp1 = false;
+	this.caseJump = true;
+	this.caseJump2 = true;
+	this.sword = false;
 	
-	this.subindo = new Image();
-	this.subindo.src = "IMAGEM/Jogo/Pedro/Pulo/pulo1.png";
+	this.up = new Image();
+	this.up.src = "IMAGEM/Jogo/Pedro/Pulo/pulo1.png";
 	
-	this.descendo = new Image();
-	this.descendo.src = "IMAGEM/Jogo/Pedro/Pulo/pulo2.png";
+	this.down = new Image();
+	this.down.src = "IMAGEM/Jogo/Pedro/Pulo/pulo2.png";
 	
-	this.ganhou = new Image();
-	this.ganhou.src = "IMAGEM/JOGO/Pedro/Ganhou/pedro_ganhou.png";
+	this.won = new Image();
+	this.won.src = "IMAGEM/JOGO/Pedro/won/pedro_won.png";
 	
 	this.start = (new Date()).getTime();
 	this.current;
@@ -36,23 +36,23 @@ var Runner = function() {
 	this.animacao = new Array();
 	this.numFrames = 4;
 	this.velocidade_cavalo = 7;
-	this.espadada_subindo = new Array();
-	this.espadada_descendo = new Array();
-	this.numFrames_espadada = 3;
+	this.sword_up = new Array();
+	this.sword_down = new Array();
+	this.numFrames_sword = 3;
 	
 		for (var i = 0; i < this.numFrames; i++) {
 			this.animacao[i] = new Image();
 			this.animacao[i].src = "IMAGEM/Jogo/Pedro/Andando/pedro" + (i + 1) + ".png";
 		}
 	
-		for (var i = 0; i < this.numFrames_espadada; i++) {
-			this.espadada_subindo[i] = new Image();
-			this.espadada_subindo[i].src = "IMAGEM/Jogo/Pedro/Espadada_Subindo/pedro1_esp" + (i + 1) + ".png";
+		for (var i = 0; i < this.numFrames_sword; i++) {
+			this.sword_up[i] = new Image();
+			this.sword_up[i].src = "IMAGEM/Jogo/Pedro/sword_up/pedro1_esp" + (i + 1) + ".png";
 		}
 	
-		for (var i = 0; i < this.numFrames_espadada; i++) {
-			this.espadada_descendo[i] = new Image();
-			this.espadada_descendo[i].src = "IMAGEM/Jogo/Pedro/Espadada_Descendo/pedro2_esp" + (i + 1) + ".png";
+		for (var i = 0; i < this.numFrames_sword; i++) {
+			this.sword_down[i] = new Image();
+			this.sword_down[i].src = "IMAGEM/Jogo/Pedro/sword_down/pedro2_esp" + (i + 1) + ".png";
 		}
 
 	this.deltaTime = function() {
@@ -72,19 +72,19 @@ var Runner = function() {
 	this.cavalo_audio = new Audio();
 	this.cavalo_audio.src = "AUDIO/Jogo/Cavalo/Cavalo_Som.mp3";
 		
-	this.espadada_erro_audio = new Audio();
-	this.espadada_erro_audio.src = "AUDIO/Jogo/Espadada/espada_erro.mp3";
+	this.sword_erro_audio = new Audio();
+	this.sword_erro_audio.src = "AUDIO/Jogo/sword/espada_erro.mp3";
 		
-	this.espadada_audio = new Audio();
-	this.espadada_audio.src = "AUDIO/Jogo/Espadada/espadada.mp3";
+	this.sword_audio = new Audio();
+	this.sword_audio.src = "AUDIO/Jogo/sword/sword.mp3";
 		
 	this.cavalo_pulo_audio = new Audio();
 	this.cavalo_pulo_audio.src = "AUDIO/Jogo/Cavalo/pulo_cavalo.mp3";
 		
 		this.pulo = function() {
 			if (this.y + this.h >= 496) {
-				this.moveup = true;
-				this.moveup1 = true;
+				this.moveUp = true;
+				this.moveUp1 = true;
 			}
 		}
 		
@@ -96,9 +96,9 @@ var Runner = function() {
 		}
 		
 		this.update = function() {
-			if (this.moveup) {
+			if (this.moveUp) {
 				if (this.y > 200) {
-					this.y -= this.speed_y;
+					this.y -= this.speedY;
 				}
 			} else {
 				if (this.y + this.h < 496) {
@@ -107,69 +107,69 @@ var Runner = function() {
 			}
 
 			if (this.y <= 200) {
-				this.moveup = false;
+				this.moveUp = false;
 			}
 			
-			if (this.moveup1) {
-				if (this.colisao_y > 210) {
-					this.colisao_y -= this.speed_y;
+			if (this.moveUp1) {
+				if (this.colliderY > 210) {
+					this.colliderY -= this.speedY;
 				}
 			} else {
-				if (this.colisao_y + this.h < 496) {
-					this.colisao_y += this.weight;
+				if (this.colliderY + this.h < 496) {
+					this.colliderY += this.weight;
 				}
 			}
 
-			if (this.colisao_y <= 210) {
-				this.moveup1 = false;
+			if (this.colliderY <= 210) {
+				this.moveUp1 = false;
 			}
 
 			if (this.y + this.h <= 496) {
-				this.pulo_case = false;
+				this.caseJump = false;
 			} else {
-				this.pulo_case = true;
-				this.espadada = false;
+				this.caseJump = true;
+				this.sword = false;
 			}
 
 			if (this.y <= 200) {
-				this.pulo_case2 = false;
+				this.caseJump2 = false;
 			}
 
 			if (this.y + this.h >= 496) {
-				this.pulo_case2 = true;
+				this.caseJump2 = true;
 			}
 			
-			if(!this.pulo_case && !this.pulo_case2) {
-				this.colisao_h = 90;
+			if(!this.caseJump && !this.caseJump2) {
+				this.colliderH = 90;
 			} else {
-				this.colisao_h = 116;
+				this.colliderH = 116;
 			}
 
 			if(this.x < 100) {
-				this.x += this.speed_x
+				this.x += this.speedX
 			} else {
-				this.speed_x = 0
+				this.speedX = 0
 			}
 
-			if(tela.WIN) {
-				this.speed_x = 4;
-			}
+			/*if(tela.WIN) {
+				this.speedX = 4;
+			}*/
 			
-			if(tela.WIN && tela.scene == "FASE1" || tela.WIN && tela.scene == "FASE2") {
+			/*if(tela.WIN && tela.scene == "FASE1" || tela.WIN && tela.scene == "FASE2") {
 				if(this.x < 840) {
-					this.x += this.speed_x;
+					this.x += this.speedX;
 				}  else {
-					this.speed_x = 0;
+					this.speedX = 0;
 				}
-			}
+			}*/
 			
-			if(tela.WIN && tela.scene == "FASE3") {
+			/*if(tela.WIN && tela.scene == "FASE3") {
 				if(this.x < 337) {
-					this.x += this.speed_x;
+					this.x += this.speedX;
 				}  else {
-					this.speed_x = 0;
+					this.speedX = 0;
 				}
-			}
+			}*/
 			
 			this.update_animacao();
 		}	
@@ -178,44 +178,44 @@ var Runner = function() {
 		
 		
 		this.draw = function() {
-			if( tela.scene == "FASE3") {
-				if (this.pulo_case && this.x < 337) {
-					/*context.fillStyle = "WHITE";
-					context.fillRect(this.colisao_x, this.colisao_y, this.colisao_w, this.colisao_h);*/
+			/*if( tela.scene == "FASE3") {
+				if (this.caseJump && this.x < 337) {
+					context.fillStyle = "RED";
+					context.fillRect(this.colliderX, this.colliderY, this.colliderW, this.colliderH);
 					context.drawImage(this.animacao[Math.floor(this.currentFrame) % this.numFrames], this.x, this.y, this.w, this.h);
 				}
-			}
+			}*/
 			
-			if( tela.scene == "FASE1" || tela.scene == "FASE2" ) {
-				if (this.pulo_case) {
-					/*context.fillStyle = "WHITE";
-					context.fillRect(this.colisao_x, this.colisao_y, this.colisao_w, this.colisao_h);*/
+			//if( tela.scene == "FASE1" || tela.scene == "FASE2" ) {
+				if (this.caseJump) {
+					context.fillStyle = "RED";
+					context.fillRect(this.colliderX, this.colliderY, this.colliderW, this.colliderH);
 					context.drawImage(this.animacao[Math.floor(this.currentFrame) % this.numFrames], this.x, this.y, this.w, this.h);
 				}
-			}
+			//}
 
-			if (!this.pulo_case && this.pulo_case2 && !this.espadada) {
-				/*context.fillStyle = "WHITE";
-				context.fillRect(this.colisao_x, this.colisao_y, this.colisao_w, this.colisao_h);*/
-				context.drawImage(this.subindo, this.x, this.y, this.w, this.h);
+			if (!this.caseJump && this.caseJump2 && !this.sword) {
+				context.fillStyle = "RED";
+				context.fillRect(this.colliderX, this.colliderY, this.colliderW, this.colliderH);
+				context.drawImage(this.up, this.x, this.y, this.w, this.h);
 			}
 			
-			if (!this.pulo_case && !this.pulo_case2 && !this.espadada) {
-				/*context.fillStyle = "WHITE";
-				context.fillRect(this.colisao_x, this.colisao_y, this.colisao_w, this.colisao_h);*/
-				context.drawImage(this.descendo, this.x, this.y, this.w, this.h);
+			if (!this.caseJump && !this.caseJump2 && !this.sword) {
+				context.fillStyle = "RED";
+				context.fillRect(this.colliderX, this.colliderY, this.colliderW, this.colliderH);
+				context.drawImage(this.down, this.x, this.y, this.w, this.h);
 			}
 			
-			if (this.espadada && this.pulo_case2) {
-				context.drawImage(this.espadada_subindo[Math.floor(this.currentFrame2) % this.numFrames_espadada], this.x + 10, this.y, this.espadada_w, this.espadada_h);
+			if (this.sword && this.caseJump2) {
+				context.drawImage(this.sword_up[Math.floor(this.currentFrame2) % this.numFrames_sword], this.x + 10, this.y, this.swordW, this.swordH);
 			}
 				
-			if (this.espadada && !this.pulo_case2) {
-				context.drawImage(this.espadada_descendo[Math.floor(this.currentFrame2) % this.numFrames_espadada], this.x + 10, this.y, this.espadada_w, this.espadada_h);
+			if (this.sword && !this.caseJump2) {
+				context.drawImage(this.sword_down[Math.floor(this.currentFrame2) % this.numFrames_sword], this.x + 10, this.y, this.swordW, this.swordH);
 			}
 			
-			if (this.x >= 337 && tela.scene == "FASE3") {
-				context.drawImage(this.subindo, this.x, this.y, this.w, this.h);
-			}
+			/*if (this.x >= 337 && tela.scene == "FASE3") {
+				context.drawImage(this.up, this.x, this.y, this.w, this.h);
+			}*/
 		}
 }	
